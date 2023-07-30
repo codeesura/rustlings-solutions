7c31aa94f18515add2cf38a5eb0b3051f96305f1
codeesura <120671243+codeesura@users.noreply.github.com>
2023-07-30 18:18:36 +0300

    Update quiz1.rs

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -10,11 +10,14 @@
 // Write a function that calculates the price of an order of apples given
 // the quantity bought. No hints this time!
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
-
+fn calculate_price_of_apples(price:i32) -> i32 {
+    if price <= 40 {
+        return price * 2;
+    } else {
+        return price
+    }
+}
 // Don't modify this function!
 #[test]
 fn verify_test() {
